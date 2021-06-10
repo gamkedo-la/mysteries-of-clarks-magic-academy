4ef0ac8b906d4f3f4e64a769ebd43f88e40d7275
cubnoble <cub.noble@yahoo.fr>
2021-06-10 14:39:39 -0500

    UI Updated for MC Attacks.

--- Assets/SpellDescription.cs
+++ Assets/SpellDescription.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class SpellDescription : MonoBehaviour, IPointerEnterHandler
+public class SpellDescription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject descriptionBox;
     public string descrption;
     public string cost;
     public bool HP;
+
+    public bool DADA, Trans, Charms, Potions, Health;
 
     public Text descriptionText, costText, HPText;
 
@@ -21,22 +23,49 @@ public class SpellDescription : MonoBehaviour, IPointerEnterHandler
         }
         else
             HPText.text = "MP";
+
     }
 
-    public void OnMouseEnterFunction()
+    public void OnPointerEnter(PointerEventData eventData)
     {
         descriptionBox.SetActive(true);
         descriptionText.text = descrption;
         costText.text = cost;
+
+        if (DADA)
+        {
+            descriptionBox.GetComponent<Image>().color = Color.yellow;
+        }
+
+        else if (Trans)
+        {
+            descriptionBox.GetComponent<Image>().color = Color.red;
+        }
+
+        else if (Charms)
+        {
+            descriptionBox.GetComponent<Image>().color = Color.blue;
+        }
+
+        else if (Potions)
+        {
+            descriptionBox.GetComponent<Image>().color = Color.green;
+        }
+        else if (Health)
+        {
+            descriptionBox.GetComponent<Image>().color = Color.cyan;
+        }
+
         if (HP)
         {
             HPText.text = "HP";
         }
         else
             HPText.text = "MP";
+
     }
 
-    public void OnMouseExitFunction()
+    public void OnPointerExit(PointerEventData eventData)
     {
         descriptionBox.SetActive(false);
     }
