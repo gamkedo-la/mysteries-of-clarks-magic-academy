1f36815ea2e19f57b95c750e1f64b6b02c5111eb
cubnoble <cub.noble@yahoo.fr>
2021-08-13 20:21:29 -0500

    Adding Holding Rooms/Party Add Drop for First 2 Dungeons.

---
+++ Assets/HoldingRoomToggle.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingRoomToggle : MonoBehaviour
+{
+    public GameObject[] Rooms;
+
+    private void Update()
+    {
+        if (GameManager.month == 4 && (GameManager.day >= 21 && GameManager.day <= 25))
+        {
+            Rooms[0].SetActive(true);
+        }
+
+        else if (GameManager.month == 4 && (GameManager.day >= 28 && GameManager.day <= 30))
+        {
+            Rooms[1].SetActive(true);
+        }
+
+        else if(GameManager.month == 5 && (GameManager.day >= 1 && GameManager.day <= 2))
+        {
+            Rooms[1].SetActive(true);
+        }
+
+        else if(GameManager.month == 5 && (GameManager.day >= 5 && GameManager.day <= 16))
+        {
+            Rooms[2].SetActive(true);
+        }
+
+        else if(GameManager.month == 5 && (GameManager.day >= 18 && GameManager.day <= 22))
+        {
+            Rooms[3].SetActive(true);
+        }
+
+        else if(GameManager.month == 5 && (GameManager.day >= 29 && GameManager.day <= 31))
+        {
+            Rooms[4].SetActive(true);
+        }
+    }
+}
