93c5f15c3ff975ba7dbe06ed12147563fd61b7c1
cubnoble <cub.noble@yahoo.fr>
2021-08-25 09:47:00 -0500

    Adjusting the Intro screen. Tying in Title and Intro to flow into Charms.

--- Assets/ButtonAdvTime.cs
+++ Assets/ButtonAdvTime.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonAdvTime : MonoBehaviour
 {
     Animator datePlay;
+    public Animator mc;
 
     private void Start()
     {
@@ -15,12 +17,15 @@ public class ButtonAdvTime : MonoBehaviour
     public void AdvTime()
     {
         StartCoroutine(StatsWaiting());
-
+        this.GetComponent<Button>().interactable =false;
+        mc.SetBool("isExcited", true);
     }
 
     IEnumerator StatsWaiting()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1f);
+        mc.SetBool("isExcited", false);
+        yield return new WaitForSeconds(2f);
         GameManager.instance.CanvasForStats.SetActive(false);
         datePlay.SetBool("ToPlay", true);
         StartCoroutine(Waiting());
@@ -31,12 +36,13 @@ public class ButtonAdvTime : MonoBehaviour
     {
         yield return new WaitForSeconds(2.1f);
         datePlay.SetBool("ToPlay", false);
-        StartCoroutine(LoadRoomWait());
+       // StartCoroutine(LoadRoomWait());
     }
-
+/*
     IEnumerator LoadRoomWait()
     {
         yield return new WaitForSeconds(.1f);
         SceneManager.LoadScene("CharmsClassroom");
     }
+*/
 }
