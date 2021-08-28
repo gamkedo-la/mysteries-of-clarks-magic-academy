28c70e46f077aa4aa9baf139ee55c3f38a19a4e8
Luis Montaña <lmontana@live.com>
2021-08-27 20:03:27 -0500

    Added full functionality to store

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -9,8 +9,10 @@ public class StoreManager : MonoBehaviour
 	public InventoryObject storeInventory;
 	public Transform storeInventoryContents;
 	public GameObject storeItemPrefab;
+    public Text playerMoney;
 	private const int SINVENTORY_ITEM_X_OFFSET = 90;
     private const int SINVENTORY_ITEM_Y_OFFSET = 30;
+    private bool buyingItem; // to avoid multiclick issue
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +20,12 @@ public class StoreManager : MonoBehaviour
         {
             GameObject item = Instantiate(storeItemPrefab, Vector3.zero, Quaternion.identity, storeInventoryContents);
             item.GetComponent<RectTransform>().localPosition = new Vector3(SINVENTORY_ITEM_X_OFFSET, -SINVENTORY_ITEM_Y_OFFSET * (i + 1), 0);
-				item.GetComponent<Text>().text = storeInventory.Container[i].item.itemName;
+			item.GetComponent<Text>().text = storeInventory.Container[i].item.itemName;
             item.transform.GetChild(0).GetComponent<Text>().text = "x" + storeInventory.Container[i].amount;
-		  }
+            item.transform.GetChild(1).GetComponent<Text>().text = "$ " + storeInventory.Container[i].item.price;
+            item.transform.GetChild(3).GetComponent<Button>().onClick.AddListener( delegate { BuyItem(item.transform.GetSiblingIndex()); });
+		}
+        playerMoney.text = "$ " + GameManager.Money;
     }
 
     // Update is called once per frame
@@ -29,9 +34,40 @@ public class StoreManager : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P)){
             ToggleStore();
         }
+        if(Input.GetKeyUp(KeyCode.Mouse0)){
+            buyingItem = false;
+        }
     }
 
-	 public void ToggleStore() {
+    public void BuyItem (int index) {
+        ItemObject item = storeInventory.Container[index].item;
+        if(GameManager.Money >= item.price && !buyingItem) {
+            buyingItem = true;
+            bool outOfStock = storeInventory.Container[index].RemoveAmount(1);
+            GameManager.instance.inventory.AddItem(item, 1);
+            GameManager.Money -= item.price;
+            if(outOfStock) {
+                storeInventory.RemoveItem(index);
+                Destroy(storeInventoryContents.transform.GetChild(storeInventory.Container.Count ).gameObject); // Remove last element
+            }
+            UpdateStore();
+        }
+    }
+
+    public void UpdateStore() {
+        for (int i = 0; i < storeInventory.Container.Count ; i++)
+        {
+            GameObject item = storeInventoryContents.GetChild(i).gameObject;
+            item.GetComponent<RectTransform>().localPosition = new Vector3(SINVENTORY_ITEM_X_OFFSET, -SINVENTORY_ITEM_Y_OFFSET * (i + 1), 0);
+			item.GetComponent<Text>().text = storeInventory.Container[i].item.itemName;
+            item.transform.GetChild(0).GetComponent<Text>().text = "x" + storeInventory.Container[i].amount;
+            item.transform.GetChild(1).GetComponent<Text>().text = "$ " + storeInventory.Container[i].item.price;
+            item.transform.GetChild(3).GetComponent<Button>().onClick.AddListener( delegate { BuyItem(item.transform.GetSiblingIndex()); });
+		}
+        playerMoney.text = "$ " + GameManager.Money;
+    }
+
+	public void ToggleStore() {
         if(Time.timeScale == 1){
             Time.timeScale = 0;
             storePanel.SetActive(true);
