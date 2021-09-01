9e5a73586cc4bd9936b84c546afee242dfb1c4fa
stebssbets <stebssbets@yahoo.com>
2021-09-01 11:45:16 +0200

    Stone footsteps implemented

--- Assets/PlayFootStepScript.cs
+++ Assets/PlayFootStepScript.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayFootStepScript : MonoBehaviour
+public class PlayFootstepScript : MonoBehaviour
 {
     
-    
+    public void PlayFootstepSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/StoneSteps");
+    }
 }
