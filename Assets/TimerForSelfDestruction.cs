c8f0de1c3ea64e5a25b8b8a9fd7c2ca63b2a170b
Christer Kaitila <breakdance@mcfunkypants.com>
2021-09-20 10:57:42 -0700

    tutorial and tooltip prefabs [wip]

--- Assets/TimerForSelfDestruction.cs
+++ Assets/TimerForSelfDestruction.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class TimerForSelfDestruction : MonoBehaviour
 {
-    private float timerDuration = 5.0f;
+    public float timerDuration = 5.0f;
+    public float slideInTime = 0.5f;
+    public float slideOutTime = 0.5f;
+    public float slideDistance = 600f;
     private float elapsedTime = 0.0f;
+    private Vector3 startPos;
+    private RectTransform myRectTransform;
 
     private void Start()
     {
         if (!GameManager.pauseMenuTutorialTipHasBeenShown)
         {
+            Debug.Log("Showing tooltip!");
+            //startPos = transform.position; // doesn't work for UI
+            myRectTransform = GetComponent<RectTransform>();
+            startPos = myRectTransform.localPosition;
             gameObject.SetActive(true);
         }
     }
@@ -18,6 +27,21 @@ public class TimerForSelfDestruction : MonoBehaviour
     void Update()
     {
         elapsedTime += Time.deltaTime;
+        
+        // none of the sliding in/out works:
+        // sliding in
+        if (elapsedTime < slideInTime) {
+            //transform.position = new Vector3(startPos.x-(slideDistance*(1f-elapsedTime/slideInTime)),startPos.y,startPos.z);
+            //myRectTransform.anchoredPosition  = new Vector3(startPos.x-(slideDistance*(1f-elapsedTime/slideInTime)),startPos.y,startPos.z);
+            //Debug.Log("Sliding in tooltip:"+transform.position.x);
+        }
+        // sliding out
+        else if (elapsedTime < (timerDuration-slideOutTime)) {
+            //transform.position = new Vector3(startPos.x - (slideDistance * (elapsedTime/(timerDuration-slideOutTime))),startPos.y,startPos.z);
+            //myRectTransform.anchoredPosition  = new Vector3(startPos.x - (slideDistance * (elapsedTime/(timerDuration-slideOutTime))),startPos.y,startPos.z);
+            //Debug.Log("Sliding out tooltip:"+transform.position.x);
+        }
+        // done
         if (elapsedTime >= timerDuration)
         {
             Destroy(gameObject);
