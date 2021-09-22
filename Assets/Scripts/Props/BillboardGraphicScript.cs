39ffe4fb1ae1fc1dfc7e492cc1797d3804e3058b
Tyler Funk <tcfunk25@gmail.com>
2021-09-22 13:28:13 -0500

    billboard The Moon for better perspective; add some options to billboard script

--- Assets/Scripts/Props/BillboardGraphicScript.cs
+++ Assets/Scripts/Props/BillboardGraphicScript.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BillboardGraphicScript : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
+    [SerializeField] bool lockXZ = true;
+    [SerializeField] bool invertY = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!mainCamera)
+        {
+            mainCamera = Camera.main;
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        gameObject.transform.LookAt(mainCamera.transform);
+        if (mainCamera)
+        {
+            gameObject.transform.LookAt(mainCamera.transform);
+            var flip = invertY ? 180f : 0f;
 
-        gameObject.transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y + 180f, 0f);
+            if (lockXZ)
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y + flip, 0f);
+            }
+            else if (invertY)
+            {
+                gameObject.transform.Rotate(Vector3.up, flip);
+            }
+            
+        }
     }
 }
